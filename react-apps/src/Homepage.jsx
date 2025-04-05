59bc97add26bb20d244f19274313d594a4f05307
Gurmesa <gurmesakedir96@gmail.com>
2025-04-05 21:54:38 +0300

    trying

--- react-apps/src/Homepage.jsx
+++ react-apps/src/Homepage.jsx
@@ -7,6 +7,7 @@ import './index.css';
     return(
       <div className="homepage">
         <h2 className="text-3xl font-bold underline">Gurmesa</h2>
+        <h1 className="BG-RED-950 text-4xl text-blue-950">KEDIR</h1>
       </div>
    )
     }
